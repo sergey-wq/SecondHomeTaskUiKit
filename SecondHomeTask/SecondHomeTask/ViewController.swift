e50a9448b48d3037bd20aaa2281a9ba5622edc80
sergeykin <sergeykin1997@mail.ru>
2022-03-01 20:32:05 +0300

    feat: added project

---
+++ SecondHomeTask/SecondHomeTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondHomeTask
+//
+//  Created by Sergey Runovich on 1.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
